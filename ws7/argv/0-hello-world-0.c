f093becad09a71551f4f030253c4c87a314686ce
Ilya w-495 Nikitin <w@ew-495.ru>
2013-12-30 16:20:32 +0400

    changes

--- ws7/argv/0-hello-world-0.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(){
-  printf("Hey, dudes!\n");
-  return 0;
-}
-
--- ws7/argv/0-hello-world-1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char* argv[], char* env[]){
-  printf("Hey, dudes!\n");
-  return 0;
-}
-
